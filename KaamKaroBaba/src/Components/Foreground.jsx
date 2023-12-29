7ce7bf3afd66bf94a0900e00474d07136f8b3926
Kunal Khare <142235997+kunal-khare-trqauto@users.noreply.github.com>
2023-12-29 17:08:10 +0530

    Half Done

---
+++ KaamKaroBaba/src/Components/Foreground.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Foreground() {
+  return (
+    <>
+     <div className='fixed  top-0 left-0 z-[3] w-full h-full'>
+<div className='w-60 h-72 bg-[#3f4f868a] rounded-[30px]'>
+
+</div>
+</div>
+    </>
+  )
+}
+
+export default Foreground
